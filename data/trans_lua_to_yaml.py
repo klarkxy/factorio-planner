f0226a5003c77c782eac41b100c1d8741d756411
klarkxy <278370456@qq.com>
2024-11-13 19:02:05 +0800

    增加一个locale版本

--- data/trans_lua_to_yaml.py
+++ data/trans_lua_to_yaml.py
@@ -1,12 +1,11 @@
 import os
+import copy
 import yaml
 from lupa import LuaRuntime
 
-PROJECT = "raw"
 
 PWD = os.path.dirname(os.path.abspath(__file__))
-RAW_LUA = os.path.join(PWD, f'{PROJECT}.lua')
-YAML = os.path.join(PWD, f'{PROJECT}.yaml')
+RAW_LUA = os.path.join(PWD, 'raw.lua')
 
 lua = LuaRuntime(unpack_returned_tuples=True)
 
@@ -36,12 +35,80 @@ def lua_table_to_dict(lua_table):
 
 python_dict = lua_table_to_dict(lua_table)
 
-with open(YAML, "w", encoding="utf-8") as yaml_file:
-    yaml.dump(python_dict, yaml_file, allow_unicode=True)
+# 生成原始yaml文件
+RAW_YAML = os.path.join(PWD, 'raw.yaml')
+with open(RAW_YAML, "w", encoding="utf-8") as f:
+    yaml.dump(python_dict, f, allow_unicode=True)
 
-recipes = python_dict['recipe']
+raw_recipes = python_dict['recipe']
 
-REPICES_FILE = os.path.join(PWD, 'recipes.yaml')
+# 生成配方yaml
+RAW_REPICES_FILE = os.path.join(PWD, 'raw_recipes.yaml')
+with open(RAW_REPICES_FILE, "w", encoding="utf-8") as f:
+    yaml.dump(raw_recipes, f, allow_unicode=True)
 
-with open(REPICES_FILE, "w", encoding="utf-8") as yaml_file:
-    yaml.dump(recipes, yaml_file, allow_unicode=True)
+recipes = {}
+recipes_zh = {}
+items_list = {}
+
+# 读取locale文件 - 中文翻译
+LOCALE_FILE = os.path.join(PWD, 'locale.yaml')
+if os.path.exists(LOCALE_FILE):
+    with open(LOCALE_FILE, "r", encoding="utf-8") as f:
+        locale = yaml.load(f.read(), Loader=yaml.FullLoader)
+else:
+    locale = {}
+
+def get_locale(k : str) -> str:
+    if k not in locale:
+        locale[k] = k 
+    return locale[k]
+
+# 提取出配方中的数据
+for k, v in raw_recipes.items():
+    name = k
+    name_zh = get_locale(name)
+    time = v.get('energy_required', 0)
+
+    # 分析原料
+    ingredients = {}
+    ingredients_zh = {}
+    for x in v.get('ingredients', {}):
+        ingredients[x['name']] = x['amount']
+        if x['name'] in locale:
+            ingredients_zh[get_locale(x['name'])] = x['amount']
+        else:
+            ingredients_zh[x['name']] = x['amount']
+    # 分析产物
+    results = {}
+    results_zh = {}
+    for x in v.get('results', {}):
+        results[x['name']] = x['amount']
+        if x['name'] in locale:
+            results_zh[get_locale(x['name'])] = x['amount']
+        else:
+            results_zh[x['name']] = x['amount']
+    recipes[name] = {
+        'time': time,
+        'ingredients': ingredients,
+        'results': results,
+    }
+    recipes_zh[name_zh] = {
+        '用时': time,
+        '原料': ingredients_zh,
+        '产品': results_zh,
+    }
+
+# 输出配方yaml
+RECIPES_FILE = os.path.join(PWD, 'recipes.yaml')
+with open(RECIPES_FILE, "w", encoding="utf-8") as f:
+    yaml.dump(recipes, f, allow_unicode=True)
+    
+# 输出中文配方表
+RECIPES_ZH_FILE = os.path.join(PWD, 'recipes_zh.yaml')
+with open(RECIPES_ZH_FILE, "w", encoding="utf-8") as f:
+    yaml.dump_all(recipes_zh, f, allow_unicode=True)
+
+# 覆写locale文件，用于手动添加汉化
+with open(LOCALE_FILE, "w", encoding="utf-8") as f:
+    yaml.dump(locale, f, allow_unicode=True)
